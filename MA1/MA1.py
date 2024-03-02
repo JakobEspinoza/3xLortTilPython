209bdba70aefa6aebadbd3551c145d406cf21a43
vsc879 <vsc879@alumni.ku.dk>
2024-03-02 12:09:52 +0100

    read dow tickers

--- MA1/MA1.py
+++ MA1/MA1.py
@@ -2,4 +2,9 @@
 
 # imports
 import numpy as np
+import pandas as pd
+
+# get down jones tickers
+djones_data = pd.read_excel(r'holdings-daily-us-en-dia.xlsx', skiprows=4)
+djones_ticks = list(djones_data['Ticker']) 
 
